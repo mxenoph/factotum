852758ec2023205c26d29410a548100e21451876
christinazavou <czavou01@gmail.com>
2020-04-26 15:16:53 +0200

    fixed workspace path

--- webpack-react-tutorial/src/App.js
+++ webpack-react-tutorial/src/App.js
@@ -1,30 +1,34 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import { Route, Link,Switch, BrowserRouter as Router } from 'react-router-dom'
-import HomePage from './js/pages/home'
-import Productivity from './js/pages/Productivity'
-import About from './js/pages/about'
-import MentalHealth from './js/pages/MentalHealth/MentalHealth'
-import WorkstationAssess from './js/pages/WorkstationAssess'
-import VertGuide from './js/pages/VertGuide'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Route, Link, Switch, BrowserRouter as Router } from "react-router-dom";
+import HomePage from "./js/pages/home";
+import Productivity from "./js/pages/Productivity";
+import About from "./js/pages/about";
+import MentalHealth from "./js/pages/MentalHealth/MentalHealth";
+import WorkstationAssess from "./js/pages/WorkstationAssess";
+import VertGuide from "./js/pages/VertGuide";
 
 class App extends Component {
-    render() {
-      return (
-        <div className="App">
+  render() {
+    return (
+      <div className="App">
         <Router>
-         <Switch>
-            <Route  path="/" exact component={() => <HomePage />}/>
+          <Switch>
+            <Route path="/" exact component={() => <HomePage />} />
             {/* <Route  path="/about" exact component={() => <About />}/> */}
-            <Route  path="/mental" exact component={() => <MentalHealth />}/>
-            <Route  path="/productivity" exact component={() => <Productivity />}/>
-            <Route  path="/workstation" exact component={() => <WorkstationAssess />}/>
-            <Route  path="/vertguide" exact component={() => <VertGuide />}/>
-            </Switch>
-        </Router>          
-        </div>
-      )
-    }
+            <Route path="/mental" exact component={() => <MentalHealth />} />
+            <Route
+              path="/productivity"
+              exact
+              component={() => <Productivity />}
+            />
+            <Route path="/workstation" exact component={() => <VertGuide />} />
+            {/* <Route  path="/vertguide" exact component={() => <VertGuide />}/> */}
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
